35851f1f2fd26825ad6b713beaf2a852a7a952b9
Vishal Mishra <144052482+Vishalmishra02@users.noreply.github.com>
2023-10-08 05:29:18 +0530

    This is my Android Quiz Project

---
+++ app/src/main/java/com/example/myquizapp/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Are you sure CodeSoft provide certificate to you"
+    };
+
+    public static String choices[][] ={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Yes","Maybe","No","Other"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Yes"
+
+    };
+}
